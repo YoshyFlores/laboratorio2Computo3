e1f63a4840e537a9099f1d144c5cc54591d86425
YoshyFlores <yoshy.floresg17@gmail.com>
2023-05-23 10:15:01 -0600

    Cambios Categoria

---
+++ laboratorio2/app/Models/Categoria.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Categoria
+ *
+ * @property $id
+ * @property $nombreCategoria
+ * @property $descripcion
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Categoria extends Model
+{
+    
+    static $rules = [
+		'nombreCategoria' => 'required',
+		'descripcion' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombreCategoria','descripcion'];
+
+
+
+}
